22cab4672cf95729ab893e78bf5342d6280efcbb
ahmadmufti1 <muftiahmad617@gmail.com>
2026-04-28 15:16:31 +0700

    Membuat function showNim dalam class mahasiswa

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 class mahasiswa{ //Mendeklarasi Class mahasiswa
-
+    public:
+    int nim;
+    void showNim(){ //Membuat function showNim
+        cout<<"No Induk ="<<nim<<endl;
+    }
 };
-
